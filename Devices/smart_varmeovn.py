675014b09b5ec070b45af94cb7f9bb94079b427d
AltinHiof <143328666+AltinHiof@users.noreply.github.com>
2024-10-26 19:32:30 +0200

    Update smart_varmeovn.py

--- Devices/smart_varmeovn.py
+++ Devices/smart_varmeovn.py
@@ -1,17 +1,17 @@
 from Deivce import Device
 
-class SmartVarmeovn(Enhet):
-    def __init__(self, prod_id, navn, brand, temperatur=20):
-        super().__init__(prod_id, navn, brand)
+class SmartVarmeovn(Device):
+    def __init__(self, prod_id, name, brand, temperatur=20):
+        super().__init__(prod_id, name, brand)
         self.temperatur = temperatur
-        self.på = False
+        self.on = False
         
     def skru_på(self):
-        self.på = True
+        self.on = True
         print(f"{self.navn} er nå på.")
 
     def skru_av(self):
-        self.på = False
+        self.on = False
         print(f"{self.navn} er nå av.")
 
     def sett_temperatur(self, ny_temp):
@@ -19,14 +19,14 @@ class SmartVarmeovn(Enhet):
             ny_temp = int(ny_temp)
             if 15 <= ny_temp <= 30:
                 self.temperatur = ny_temp
-                print(f"{self.navn} er nå satt til {self.temperatur} grader.")
+                print(f"{self.name} er nå satt til {self.temperatur} grader.")
             else:
                 print("Ugyldig temperatur! Temperaturen må være mellom 15 og 30 grader.")
         except ValueError:
             print("Vennligst skriv inn en gyldig heltallverdi for temperaturen.")
 
     def status(self):
-        if self.på:
-            print(f"{self.navn} ({self.brand}) er på, temperatur: {self.temperatur} grader.")
+        if self.on:
+            print(f"{self.name} ({self.brand}) er på, temperatur: {self.temperatur} grader.")
         else:
-            print(f"{self.navn} ({self.brand}) er av.")
+            print(f"{self.name} ({self.brand}) er av.")
