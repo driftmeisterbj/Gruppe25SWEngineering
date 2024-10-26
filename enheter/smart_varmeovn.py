c6e99642b154c3c92cef427b2a9ea6060c17c540
AltinHiof <143328666+AltinHiof@users.noreply.github.com>
2024-10-26 19:35:24 +0200

    Update smart_varmeovn.py

--- enheter/smart_varmeovn.py
+++ enheter/smart_varmeovn.py
@@ -1,23 +1,25 @@
-from enhet import Enhet
+from Device import Device
 
-class SmartVarmeovn(Enhet):
-    def __init__(self, navn, temperatur=20):
-        super().__init__(navn)
+class SmartVarmeovn(Device):
+    def __init__(self, prod_id, name, brand, category, temperatur=20):
+        super().__init__(name, prod_id, brand, category)
         self.temperatur = temperatur
+        self.on = False
 
     def sett_temperatur(self, ny_temp):
         try:
             ny_temp = int(ny_temp)
             if 15 <= ny_temp <= 30:
                 self.temperatur = ny_temp
-                print(f"{self.navn} er nå satt til {self.temperatur} grader.")
+                print(f"{self.name} er nå satt til {self.temperatur} grader.")
             else:
                 print("Ugyldig temperatur! Temperaturen må være mellom 15 og 30 grader.")
+                
         except ValueError:
-            print("Vennligst skriv inn en gyldig heltallverdi for temperaturen.")
+            raise ValueError("Vennligst skriv inn en gyldig heltallverdi for temperaturen.")
 
     def status(self):
-        if self.på:
-            print(f"{self.navn} er på, temperatur: {self.temperatur} grader.")
+        if self.on:
+            return f"{self.name} ({self.brand}) er på, temperatur: {self.temperatur} grader."
         else:
-            print(f"{self.navn} er av.")
+            return f"{self.name} ({self.brand}) er av."
